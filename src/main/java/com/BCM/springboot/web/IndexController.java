89b6c07408a20b382cfb4c89b5b507ebbf9a354e
danpung2 <danpung_2@naver.com>
2021-12-19 13:55:21 +0900

    FEAT Google Login

--- src/main/java/com/BCM/springboot/web/IndexController.java
+++ src/main/java/com/BCM/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.BCM.springboot.web;
 
+import com.BCM.springboot.config.auth.LoginUser;
+import com.BCM.springboot.config.auth.dto.SessionUser;
 import com.BCM.springboot.service.posts.PostsService;
 import com.BCM.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findALLDesc());
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -32,10 +40,4 @@ public class IndexController {
 
         return "posts-update";
     }
-//    @GetMapping("/")
-//    public String index(){
-//        return "index";
-//    }
-//
-
 }
