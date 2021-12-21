1413cb64d0b096da6cd26c25036270475a80acf6
danpung2 <danpung_2@naver.com>
2021-12-21 21:28:36 +0900

    MODIFIED

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=business-card-management
+PROJECT_NAME=BusinessCardManagement
 
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
